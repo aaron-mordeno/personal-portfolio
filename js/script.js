e278235e994b6c89f531cc2df130262fb6b9b958
aaron_mordeno <62050481+aaron-mordeno@users.noreply.github.com>
2023-04-17 21:03:01 +0800

    Added project files
    Files from my Treehouse course portfolio project

---
+++ js/script.js
@@ -0,0 +1,95 @@
+/*!
+ * Start Bootstrap - Resume v4.0.0-beta.2 (https://startbootstrap.com/template-overviews/resume)
+ * Copyright 2013-2017 Start Bootstrap
+ * Licensed under MIT (https://github.com/BlackrockDigital/startbootstrap-resume/blob/master/LICENSE)
+ */
+
+!(function (e) {
+  "use strict";
+  e('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function () {
+    if (
+      location.pathname.replace(/^\//, "") ==
+        this.pathname.replace(/^\//, "") &&
+      location.hostname == this.hostname
+    ) {
+      var t = e(this.hash);
+      if ((t = t.length ? t : e("[name=" + this.hash.slice(1) + "]")).length)
+        return (
+          e("html, body").animate(
+            { scrollTop: t.offset().top },
+            1e3,
+            "easeInOutExpo"
+          ),
+          !1
+        );
+    }
+  }),
+    e(".js-scroll-trigger").click(function () {
+      e(".navbar-collapse").collapse("hide");
+    }),
+    e("body").scrollspy({ target: "#sideNav" });
+})(jQuery);
+
+// light and dark theme toggle
+const toggleSwitch = document.getElementById("toggle-switch");
+toggleSwitch.addEventListener("click", switchTheme);
+
+function switchTheme() {
+  if (toggleSwitch.classList.contains("light-theme")) {
+    toggleSwitch.classList.remove("light-theme");
+    toggleSwitch.classList.add("dark-theme");
+    darkTheme();
+  } else if (toggleSwitch.classList.contains("dark-theme")) {
+    toggleSwitch.classList.remove("dark-theme");
+    toggleSwitch.classList.add("light-theme");
+    lightTheme();
+  }
+}
+
+function darkTheme() {
+  document.documentElement.style.setProperty("--background-color", "#343a40");
+  document.documentElement.style.setProperty("--body-color", "#fff");
+  document.documentElement.style.setProperty("--header-color", "#fff");
+  document.documentElement.style.setProperty("--card-header", "#fff");
+  document.documentElement.style.setProperty("--card-text", "#fff");
+  document.documentElement.style.setProperty("--card-border", "#fff");
+  document.documentElement.style.setProperty("--card-background", "#343a40");
+}
+
+function lightTheme() {
+  document.documentElement.style.setProperty("--background-color", "#fff");
+  document.documentElement.style.setProperty("--body-color", "#343a40");
+  document.documentElement.style.setProperty("--header-color", "#343a40");
+  document.documentElement.style.setProperty("--card-header", "#343a40");
+  document.documentElement.style.setProperty("--card-text", "#868e96");
+  document.documentElement.style.setProperty("--card-border", "#868e96");
+  document.documentElement.style.setProperty("--card-background", "#fff");
+}
+
+// form validation
+const form = document.getElementsByTagName("form");
+const name = document.getElementById("inputName");
+const subject = document.getElementById("inputSubject");
+const message = document.getElementById("inputMessage");
+const send = document.getElementById("send");
+
+send.addEventListener("click", formValidation);
+
+function formValidation() {
+  console.log("clicked");
+  if (name.value === "" || subject.value === "" || message.value === "") {
+    let errorMessage = "";
+    if (name.value === "") {
+      errorMessage += " Name";
+    }
+    if (subject.value === "") {
+      errorMessage += " Subject";
+    }
+    if (message.value === "") {
+      errorMessage += " Message";
+    }
+    alert("Please fill in:" + errorMessage);
+  } else {
+    alert("Thanks for your message!");
+  }
+}
